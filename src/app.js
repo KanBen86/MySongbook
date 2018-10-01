b4869abf720ef706385e8639e351b34a8fef6b77
KanBen86 <benjamin.kanzler@gmail.com>
2018-10-01 15:56:18 +0200

    new .js files

---
+++ src/app.js
@@ -0,0 +1,15 @@
+"use strict"
+
+/**
+* Hauptklasse der Anwendung. Kümmert isch darum, die Anwendung auszuführen
+* und die angeforderten Bildschirmseiten anzuzeigen
+*/
+class App {
+    constructor() {
+
+    }
+
+    start(){
+        console.log("Die Klasse App sagt Hallo!");
+    }
+}
